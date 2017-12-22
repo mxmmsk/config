8081f628e088cf57afcb1c7932baa877f5b4ceb5
mxmmsk <mxmmsk@gmail.com>
2017-12-22 18:34:04 +0300

    FarMacro: Show executed command in the Far editor title

--- far-macro/scripts/Panel.AltEnter.lua
+++ far-macro/scripts/Panel.AltEnter.lua
@@ -8,9 +8,17 @@ Macro {
   description="Execute command and send output to Editor";
 action = function()
   local cmdLine = CmdLine.Value
-  Keys('CtrlHome')
-  print("edit:<")
+  if mf.substr(cmdLine,0,6):lower()~="edit:<" then
+    Keys('CtrlHome')
+    print("edit:<")
+    cmdLine = "edit:<"..cmdLine
+  end
   Keys("Enter")
+  if not Area.Editor then
+    return
+  end
+  -- Plugin.SyncCall("4b675d80-1d4a-4ea9-8436-fdc23f2fc14b","Rename 0 \""..cmdLine.."\"")
+  Editor.SetTitle(cmdLine)
   if GitMacros.KnownGitAlias(cmdLine) then
     Keys("CtrlL")
   end
